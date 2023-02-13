a6166dc5c3f1fdec6b5e1bb198ddfd5e3976e63a
Renante Cajucom <cajcuom@sheridancollege.ca>
2023-02-13 17:15:43 -0500

    completed program and it works

--- src/main/java/ca/sheridan/controllers/StudentController.java
+++ src/main/java/ca/sheridan/controllers/StudentController.java
@@ -1,16 +1,20 @@
 package ca.sheridan.controllers;
 
+import ca.sheridan.database.DatabaseAccess;
 import ca.sheridan.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
-
+    @Autowired
+    private DatabaseAccess da;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("student", new Student());
+        da.insertStudent();
         return "index";
     }
 }
--- src/main/java/ca/sheridan/database/DatabaseAccess.java
+++ src/main/java/ca/sheridan/database/DatabaseAccess.java
@@ -1,6 +1,7 @@
 package ca.sheridan.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,12 @@ import org.springframework.stereotype.Repository;
 public class DatabaseAccess {
     @Autowired
     protected NamedParameterJdbcTemplate jdbc;
+
+    public void insertStudent() {
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        String query = "INSERT INTO student(name) VALUES('Frank')";
+        int rowsAffected = jdbc.update(query, namedParameters);
+        if (rowsAffected > 0)
+            System.out.println("Inserted into database.");
+    }
 }
